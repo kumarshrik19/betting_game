54c7fc34c451f2177834060cb4dce857d0a7d29c
kumarshrik <kumarshrik19@gmail.com>
2025-07-22 13:49:39 -0400

    fix

--- betting_game.py
+++ betting_game.py
@@ -180,7 +180,7 @@ odds = st.session_state.odds
 if st.session_state.submitted == True: 
     st.write(f"### Current Bankroll: ${st.session_state.bankroll-st.session_state.last_win:.2f}, Win/Loss: {st.session_state.last_win:.2f}")
 else:
-    st.write(f"### v3 Current Bankroll: ${st.session_state.bankroll:.2f}")
+    st.write(f"### v4 Current Bankroll: ${st.session_state.bankroll:.2f}")
         
 
 # ---- Expected vs Offered Odds View ----
